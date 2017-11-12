71c8d3f8106a2037eca8fede26298305bb2e1d45
mateuszroth <mateusz.roth@gmail.com>
2017-11-12 19:54:50 +0100

    Firebase cloud function to count logs in

--- functions/index.js
+++ functions/index.js
@@ -6,3 +6,13 @@ const functions = require('firebase-functions');
 // exports.helloWorld = functions.https.onRequest((request, response) => {
 //  response.send("Hello from Firebase!");
 // });
+
+exports.lastUserLoggedInCounter = functions.database.ref('/lastUserLoggedIn')
+  .onWrite(event => {
+    var eventSnapshot = event.data;
+    var countRef = eventSnapshot.ref.parent.child('lastUserLoggedInCount');
+    return countRef.transaction((value) => {
+      value = value ? (value + 1) : 1
+      return value
+    })
+  });
